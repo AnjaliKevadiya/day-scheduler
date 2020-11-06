47fe7cb432ea8bcd91b3f264fab8ed0b319adab5
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-11-06 18:17:43 -0500

    added user story and criteria in readme file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
-    var today = moment().format('MMMM Do YYYY');
-    console.log(today);
+    var todayDate = moment().format('MMMM Do YYYY');
+    console.log(todayDate);
+
+    var currentTime = moment().format('H');
+    console.log(currentTime)
+
+    var currentTime = $(this).attr("data-hour");
+    console.log(hour)
+
+    var timeBlocks = $(".input-block");
+    console.log(timeBlocks)
+    console.log(timeBlocks.length);
+
+    // currentTime = currentTime - 9;
+
+    for(var i=0; i < timeBlocks.length; i++) {
+        if (i === currentTime) {
+            timeBlocks[i].attr("class", "present");
+        } 
+        else if (i < currentTime) {
+            timeBlocks[i].attr("class", "present");
+        } else {
+            timeBlocks[i].attr("class", "future");
+        }    
+    }
 });
 
