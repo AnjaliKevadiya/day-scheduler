1c06a3206e84497c31698882953b6bbccc392242
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-11-06 22:35:07 -0500

    add tasks to the localstorage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
+
+    //Get current date and formated it
     var todayDate = moment().format('dddd, MMMM Do');
+
+    //Grab the #currentDay and set it's content to current date 
     $("#currentDay").text(todayDate);
 
+    //Get current time
     var currentTime = moment().format('H');
 
+    //Grab task object from the local storage
+    var allTasks = JSON.parse(localStorage.getItem("taskObject")) || [];
+
+    //Iterates over all the .input-block to set class
     $(".input-block").each(function() {
         var hour = parseInt($(this).prev().attr("data-hour"));
-        console.log(hour);
-        console.log(currentTime);
 
         if (hour === currentTime) {
             $(this).addClass("present");
@@ -16,6 +23,25 @@ $(document).ready(function() {
         } else {
             $(this).addClass("future");
         }    
+    });
+
+    //Save task on save button click
+    $(".btn-save").on("click", function() {
+
+        var hour = $(this).prev().prev().attr("data-hour");
+        var inputValue = $(this).prev().val().trim();
+
+        if(inputValue === "") {
+            return;
+        }
+
+        var newTask = {
+            'time': hour,
+            'description': inputValue
+        };  
+        allTasks.push(newTask);
+
+        localStorage.setItem("taskObject", JSON.stringify(allTasks));
     });
 });
 
